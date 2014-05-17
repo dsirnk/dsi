d248829b7f9c3ec283eff3c2df86d3a35b8af133
Ronak Desai <rdesai@klick.com>
2014-05-16 20:19:33 -0400

    clean-up     #to-do     - set mismatch tolerance to 0     - fix diff image rendering     - incorrect filenames to be fixed     - add livereload to the link (may not be feasible)     - segregate screefly into its own repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,123 +1,120 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-	// Metadata.
-	var pkg = grunt.file.readJSON('package.json'),
-		banner = '/*! ' + pkg.name + ' - v' + pkg.version + ' - ' +
-				grunt.template.today("yyyy-mm-dd") + '\n' +
-				pkg.homepage ? ('* ' + pkg.homepage + '\n') : '' +
-				'* Copyright (c) ' + grunt.template.today("yyyy") + ' ' +
-				pkg.author.name + ';' + ' Licensed MIT */\n',
-		config = grunt.file.readJSON('config.json'),
-		path = require('path'),
+    // Metadata.
+    var config = grunt.file.readJSON('config.json'),
+        path = require('path'),
 
-		// My custom variables
-		win = process.platform === 'win32',
-		webistes = 'webistes/',
-		webisteFiles = webistes + '**/*.+(css|js|htm|html|cshtml|gif|png|jpg|jpeg)',
+        // My custom variables
+        webistes = 'webistes/',
+        webisteFiles = webistes + '**/*.+(css|js|htm|html|cshtml|gif|png|jpg|jpeg)',
 
-		emails = 'emails/',
-		emailFiles = '**/*.+(htm|html)',
-		emailZips = emails + emailFiles + '.zip',
+        emails = 'emails/',
+        emailFiles = '**/*.+(htm|html)',
+        emailZips = emails + emailFiles + '.zip',
 
-		screenfly = 'test/**/*screenfly.js',
+        screenfly = 'test/**/*screenfly.js',
 
-		screenshots = 'screenshots/',
-		lastShots = screenshots + 'base/',
-		resultShots = screenshots + 'results/';
+        screenshots = 'screenshots/',
+        lastShots = screenshots + 'base/',
+        resultShots = screenshots + 'results/';
 
-	// load all npm grunt tasks
-	require('load-grunt-tasks')(grunt);
+    win = process.platform === 'win32',
 
-	grunt.initConfig({
+    // load all npm grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-		// jsHint all js files
-		jshint: {
-			gruntfile: {
-				src: 'Gruntfile.js'
-			},
-			emailer: {
-				src: 'tasks/**/*emailer.js',
-			},
-			phantomcss: {
-				src: screenfly,
-			},
-			all: ['*.js']
-		},
+    grunt.initConfig({
 
-		// Before generating any new files, remove any previously-created files.
-		clean: {
-			emails: [emailZips],
-			screenshots: [resultShots],
-			webistes: [webistes]
-		},
+        // jsHint all js files
+        jshint: {
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            emailer: {
+                src: 'tasks/**/*emailer.js',
+            },
+            phantomcss: {
+                src: screenfly,
+            },
+            all: ['*.js']
+        },
 
-		emailer: {
-			zip: {
-				expand: true,
-				cwd: emails,
-				src: [emailFiles],
-				misc: ['**/*.+(jpg|jpeg|gif|png)']
-			}
-		},
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            emails: [emailZips],
+            screenshots: [resultShots],
+            webistes: [webistes]
+        },
 
-		phantomcss: {
-			desktop: {
-				options: {
-					screenshots: lastShots + 'desktop/',
-					results: resultShots + 'desktop',
-					viewportSize: [1024, 768]
-				},
-				src: [screenfly]
-			},
-			mobile: {
+        emailer: {
+            zip: {
+                expand: true,
+                cwd: emails,
+                src: [emailFiles],
+                misc: ['**/*.+(jpg|jpeg|gif|png)']
+            }
+        },
+
+        phantomcss: {
+            screenshot: {
+                options: {
+                    screenshots: lastShots,
+                    results: resultShots,
+                    viewportSize: [1024]
+                },
+                src: [screenfly]
+            },
+            /**
+			screenshot_mobile: {
 				options: {
 					screenshots: lastShots + 'mobile/',
 					results: resultShots + 'mobile',
-					viewportSize: [320, 480]
+					viewportSize: [320]
 				},
 				src: [screenfly]
-			}
-		},
+			},
+			/**/
+        },
 
-		watch: {
-			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile'],
-				options: {
-					spawn: false
-				}
-			},
-			emailer: {
-				files: '<%= jshint.emailer.src %>',
-				tasks: ['jshint:emailer', 'emailer'],
-				options: {
-					// spawn: false
-				}
-			},
-			emails: {
-				files: [emailFiles],
-				tasks: ['emails'],
-				options: {
-					spawn: false
-				}
-			},
-			phantomcss: {
-				files: '<%= jshint.phantomcss.src %>',
-				tasks: ['jshint:phantomcss', 'phantomcss'],
-				options: {
-					spawn: false
-				}
-			},
-			webistes: {
-				files: webisteFiles,
-				tasks: ['phantomcss'],
-				options: {
-					spawn: false
-				}
-			}
-		},
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile'],
+                options: {
+                    spawn: false
+                }
+            },
+            emailer: {
+                files: '<%= jshint.emailer.src %>',
+                tasks: ['jshint:emailer', 'emailer'],
+                options: {
+                    // spawn: false
+                }
+            },
+            emails: {
+                files: [emailFiles],
+                tasks: ['emails'],
+                options: {
+                    spawn: false
+                }
+            },
+            phantomcss: {
+                files: '<%= jshint.phantomcss.src %>',
+                tasks: ['jshint:phantomcss', 'phantomcss'],
+                options: {
+                    spawn: false
+                }
+            },
+            webistes: {
+                files: [webisteFiles],
+                tasks: ['phantomcss'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
 
-		/* Grunt exec properties
+        /* Grunt exec properties
 		__command:__ The shell command to be executed. Must be a string or a function that returns a string. (__alias:__ cmd)
 		__stdout:__ If true, stdout will be printed. Defaults to true.
 		__stderr:__ If true, stderr will be printed. Defaults to true.
@@ -126,87 +123,64 @@ module.exports = function (grunt) {
 		__callback:__ The callback function passed child_process.exec. Defaults to a noop.
 		*/
 
-		exec: {
-			open: {
-				cmd: 'echo '
-			},
-			list_files: {
-				cmd: 'ls -l **'
-			},
-			symLink: {
-				cmd: function() {
-						var websiteDir = webistes.replace('/', path.sep),
-							websiteName, websiteDest,
-							cmd = 'mkdir ' + websiteDir;
-						config.websites.forEach(function(website) {
-							websiteName =  websiteDir + website.name;
-							websiteDest = website.dir;
-							cmd += (win ?
-								'& mklink /J ' + websiteName + ' ' + websiteDest :
-								'& ln -s ' + websiteDest + ' ' + websiteName);
-						});
-						return cmd;
-					}
-			},
-			echo_grunt_version: {
-				cmd: function() { return 'echo ' + this.version; }
-			},
-			echo_name: {
-				cmd: function(firstName, lastName) {
-					firstName = typeof firstName !== 'undefined' ? firstName : 'Ronak';
-					lastName = typeof lastName !== 'undefined' ? lastName : 'Desai';
-					var formattedName = [
-						lastName.toUpperCase(),
-						firstName.toUpperCase()
-					].join(', ');
+        exec: {
+            open: {
+                cmd: 'echo '
+            },
+            list_files: {
+                cmd: 'ls -l **'
+            },
+            symLink: {
+                cmd: function() {
+                    var websiteName, websiteDest,
+                        websiteDir = webistes.replace('/', path.sep),
+                        cmd = 'mkdir ' + websiteDir;
+                    config.websites.forEach(function(website) {
+                        websiteName = websiteDir + website.name;
+                        websiteDest = website.dir;
+                        winCmd = 'mklink /J ' + websiteName + ' ' + websiteDest;
+                        lnCmd = 'ln -s ' + websiteDest + ' ' + websiteName;
+                        cmd += ' &&\n ' + (win ? winCmd : lnCmd);
+                    });
+                    // return 'echo "' + cmd + '"';
+                    return cmd;
+                }
+            },
+        }
+    });
 
-					return 'echo ' + formattedName;
-				}
-			},
-			remove_logs: {
-				command: 'rm -f *.log',
-				stdout: false,
-				stderr: false
-			},
-		}
-	});
+    // Watch events
+    grunt.event.on('watch', function(action, filepath, target) {
+        // Re-configure tasks to only run on changed file
+        switch (target) {
+            case 'emails':
+                {
+                    grunt.config(['exec', 'open', 'cmd'], 'open "' + filepath + '"');
+                    grunt.config(['emailer', 'zip', 'cwd'], require('path').dirname(filepath));
+                    break;
+                }
+            case 'webistes':
+                {
+                    grunt.log.subhead(filepath + ' updated');
+                    break;
+                }
+        }
+    });
 
-	// Watch events
-	grunt.event.on('watch', function(action, filepath, target) {
-		// // configure copy:devTmpl to only run on changed file
-		// grunt.config(['copy','devTmpl'], filepath);
-		// // configure copy:devImg to only run on changed file
-		// grunt.config(['copy','devImg'], filepath);
-		switch(target) {
-			case 'emails': {
-				grunt.config(['exec', 'open', 'cmd'], 'open "' + filepath + '"');
-				grunt.config(['emailer', 'zip', 'cwd'], require('path').dirname(filepath));
-				break;
-			}
-			case 'webistes': {
-				grunt.log.subhead(filepath + ' updated');
-				break;
-			}
-		}
-	});
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
-
-	// By default, lint and run all tests.
-	grunt.registerTask(
-		'default',
-		'Running "DEFAULT: jshint, clean, watch"...',
-		['jshint:all', 'clean', 'watch']
-	);
-	grunt.registerTask(
-		'emails',
-		'Running Emailer - zip and upload...',
-		['clean', 'emailer', 'watch']
-	);
-	grunt.registerTask(
-		'screenshots',
-		'Taking Screenshots - crawl, screenshot and diff',
-		['clean', 'exec:symLink', 'phantomcss', 'watch']
-	);
+    // By default, lint and run all tests.
+    grunt.registerTask(
+        'default',
+        'Running "DEFAULT: jshint, clean, watch"...', ['jshint:all', 'clean', 'watch']
+    );
+    grunt.registerTask(
+        'emails',
+        'Running Emailer - zip and upload...', ['clean', 'emailer', 'watch']
+    );
+    grunt.registerTask(
+        'screenshots',
+        'Taking Screenshots - crawl, screenshot and diff', ['clean', 'exec:symLink', 'phantomcss', 'watch']
+    );
 };
--- test/screenfly.js
+++ test/screenfly.js
@@ -38,110 +38,114 @@ var startUrl = require('../config.json').websites[0].link,
     // casper = require('casper').create({ /*verbose: true, logLevel: 'debug'*/ }),
 
     // Fly by the given URL
-    screenfly = function(url) {
+    screenfly = {
+        onPage: function(url) {
 
-        // Add the URL to the visited stack
-        visitedUrls.push(url);
-        onpageUrls = [];
+            // Add the URL to the visited stack
+            visitedUrls.push(url);
+            onpageUrls = [];
 
-        url = (url.indexOf(hostUrl)!==-1 ? '' : hostUrl) + url;
+            url = (url.indexOf(hostUrl)!==-1 ? '' : hostUrl) + url;
 
-        // Open the URL
-        casper
-            .open(url)
-            .then(function initPage() {
-                var $this = casper;
+            // Open the URL
+            casper
+                .open(url)
+                .then(function initPage() {
+                    var $this = casper;
 
-                // Display the URL and status
-                statusStyle.fg = status[$this.status().currentHTTPStatus] || status['default'];
-                $this.echo($this.colorizer.format('Opened ' + url, statusStyle));
+                    // Display the URL and status
+                    statusStyle.fg = status[$this.status().currentHTTPStatus] || status['default'];
+                    $this.echo($this.colorizer.format('Opened ' + url, statusStyle));
 
-                url = $this.evaluate(function() {
-                    return {
-                        'host': window.location.origin,
-                        'filename': window.location.href
-                                .toLowerCase()
-                                .replace(window.location.origin, '')
-                                .replace(/[^\w]+/g, '-')
-                                .replace(/^-+|-+$/g, '') || 'home'
-                    };
-                });
-                hostUrl = url.host;
+                    url = $this.evaluate(function() {
+                        return {
+                            'host': window.location.origin,
+                            'filename': window.location.href
+                                    .toLowerCase()
+                                    .replace(window.location.origin, '')
+                                    .replace(/[^\w]+/g, '-')
+                                    .replace(/^-+|-+$/g, '') || 'home'
+                        };
+                    });
+                    hostUrl = url.host;
 
-                phantomcss.init({
-                    mismatchTolerance: 0.0,
-                    hideElements: '.logger',
-                    addLabelToFailedImage: true,
-                    onFail: function(test){
-                        console.log('onFail');
-                        console.log(test.filename, test.mismatch);
-                    },
-                    onPass: function(){
-                        console.log('onPass');
-                        console.log(test.filename);
-                    },
-                    onTimeout: function(){
-                        console.log('onTimeout');
-                        console.log(test.filename);
-                    },
-                    onComplete: function(allTests, noOfFails, noOfErrors){
-                        allTests.forEach(function(test){
-                            if(test.fail){
-                                console.log('onComplete');
-                                console.log(test.filename, test.mismatch);
-                            }
-                        });
-                    },
-                });
-                phantomcss.turnOffAnimations();
-                $this.echo('Taking screenshot');
-                phantomcss.screenshot(
-                    'body',
-                    0,
-                    '#fixed-isi',
-                    url.filename
-                );
-
-                // Add newly found URLs to the stack
-                pendingUrls = pendingUrls.concat(
-                    _.uniq(
-                        _.filter(
-                            // Find links present on this page
-                            $this.evaluate(function() {
-                                return Array.prototype.map.call(document.querySelectorAll('a'), function(e) {
-                                    return e.getAttribute('href');
-                                });
-                            }),
-                            // Filter redundant and external urls
-                            function(l) {
-                                if (redundantUrl.concat(pendingUrls).concat(visitedUrls).indexOf(l) !== -1) return false;
-                                if (specialUrl.indexOf(l[0]) !== -1) {
-                                    onpageUrls.push();
-                                    return false;
+                    phantomcss.init({
+                        mismatchTolerance: 0.0,
+                        hideElements: '.logger',
+                        addLabelToFailedImage: true,
+                        onFail: function(test){
+                            console.log('onFail');
+                            console.log(test.filename, test.mismatch);
+                        },
+                        onPass: function(){
+                            console.log('onPass');
+                            console.log(test.filename);
+                        },
+                        onTimeout: function(){
+                            console.log('onTimeout');
+                            console.log(test.filename);
+                        },
+                        onComplete: function(allTests, noOfFails, noOfErrors){
+                            allTests.forEach(function(test){
+                                if(test.fail){
+                                    console.log('onComplete');
+                                    console.log(test.filename, test.mismatch);
                                 }
-                                var tempLink = document.createElement('a');
-                                tempLink.href = l;
-                                // if(tempLink.hostname === window.location.hostname) $this.echo($this.colorizer.format('-> Pushed ' + l + ' onto the stack', { fg: 'magenta' }));
-                                return tempLink.hostname === window.location.hostname;
-                            }
+                            });
+                        },
+                    });
+                    phantomcss.turnOffAnimations();
+                    $this.echo('Taking screenshot');
+                    phantomcss.screenshot(
+                        'body',
+                        0,
+                        '#fixed-isi',
+                        url.filename
+                    );
+
+                    /**
+                    // Add newly found URLs to the stack
+                    pendingUrls = pendingUrls.concat(
+                        _.uniq(
+                            _.filter(
+                                // Find links present on this page
+                                $this.evaluate(function() {
+                                    return Array.prototype.map.call(document.querySelectorAll('a'), function(e) {
+                                        return e.getAttribute('href');
+                                    });
+                                }),
+                                // Filter redundant and external urls
+                                function(l) {
+                                    if (redundantUrl.concat(pendingUrls).concat(visitedUrls).indexOf(l) !== -1) return false;
+                                    if (specialUrl.indexOf(l[0]) !== -1) {
+                                        onpageUrls.push();
+                                        return false;
+                                    }
+                                    var tempLink = document.createElement('a');
+                                    tempLink.href = l;
+                                    // if(tempLink.hostname === window.location.hostname) $this.echo($this.colorizer.format('-> Pushed ' + l + ' onto the stack', { fg: 'magenta' }));
+                                    return tempLink.hostname === window.location.hostname;
+                                }
+                            )
                         )
-                    )
-                );
+                    );
+                    /**/
 
-                // If there are URLs to be processed
-                if (pendingUrls.length > 0) {
-                    var nextUrl = pendingUrls.shift();
-                    // $this.echo($this.colorizer.format('<- Popped ' + nextUrl + ' from the stack', { fg: 'blue' }));
-                    screenfly(nextUrl);
-                }
+                    // If there are URLs to be processed
+                    if (pendingUrls.length > 0) {
+                        var nextUrl = pendingUrls.shift();
+                        // $this.echo($this.colorizer.format('<- Popped ' + nextUrl + ' from the stack', { fg: 'blue' }));
+                        screenfly.onPage(nextUrl);
+                    }
 
-            });
+                });
+        }
     };
 
 // Start flying
 casper
     .start(startUrl, function() {
-        screenfly(startUrl);
+        screenfly.onPage(startUrl);
     });
 
 // Start the run
